9c54a81a20c0da88cc74db2fb93b2e17d3ae4508
rodrigopalomino <117995998+rodrigopalomino@users.noreply.github.com>
2023-06-29 17:59:20 -0500

    ya funciona

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import { dirname, join } from 'path'
+import { fileURLToPath } from "url";
 
-
+import indexRoutes from './routers/index.routes.js'
 
 
 
 const app = express()
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set('view engine', 'ejs')
+app.set('views', join(__dirname,'views'))
+app.use(express.static(join(__dirname,'style')))
+
+
+app.use(indexRoutes)
 
 app.listen(3000)
 console.log("corriendo en el puerto 3000")
