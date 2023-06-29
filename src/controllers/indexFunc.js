6d75b72149a19b3dbee517af11de0c0efd4dbc2e
PBMGC <dariogutierrez953@gmail.com>
2023-06-29 18:03:29 -0500

    funcions v1

--- src/controllers/indexFunc.js
+++ src/controllers/indexFunc.js
@@ -1,5 +1,40 @@
-function obtener(){
-    let especialidad=document.getElementById("especialidad")
+import { pool } from "../db";
 
-    console.log(especialidad)
+export function obtener() {
+  let especialidad = document.getElementById("especialidad");
+  let doctor = document.getElementById("doctor");
+  let fecha = document.getElementById("fecha");
+
+  const cita = [especialidad, doctor, fecha];
+
+  return cita;
 }
+
+export function consultarEspeDB(){
+    pool.query("Select especialidad from doctor",function(error,resultado,fields)){
+        if(error){
+            console.log(error)
+        }else{
+            console.log(resultados)
+        }
+    }
+}
+
+export function consultarDoctDB(){
+    let especialidad=document.getElementById("especialidad")
+    pool.query("Select nombre from doctor where especialidad="+especialidad,function(error,resultado,fields)){
+        if(error){
+            console.log(error)
+        }else{
+            console.log(resultados)
+        }
+    }
+}
+
+export function Fechas() {
+    pool.query("select ")
+}
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-const express = require("express");
-const { dirname } = require('path');
-const { obtener } = require("./controllers/indexFunc");
+import express from "express";
 
 const app = express();
 
 app.listen(3000);
 console.log("corriendo en el puerto 3000");
 
-obtener();
