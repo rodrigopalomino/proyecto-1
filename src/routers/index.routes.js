1d55a4cc77d8547406f1ff75af67578ea67f4cb8
PBMGC <dariogutierrez953@gmail.com>
2023-06-30 17:35:12 -0500

    corregido

--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { principal } from "../controllers/indexFunc.js";
 import { enviar } from "../controllers/indexFunc.js";
+import { getEstados } from "../controllers/estado.controllers.js";
 
 const router = Router();
 
