f27a739a5abcb11637f9e8113f2ff8582fe1c342
PBMGC <dariogutierrez953@gmail.com>
2023-06-30 17:26:45 -0500

    bloqueo de calendario

--- src/controllers/indexFunc.js
+++ src/controllers/indexFunc.js
@@ -6,29 +6,17 @@ export const principal =async (req, res) => {
   
   const fechaActual=new Date().toISOString().split('T')[0];
 
+  const rows_doctor_id=[]
+
+  const x=req.body.especialidad
+
+  console.log(x)
+
   res.render('index',{rows_doctor,fechaActual});
 };
 
-export const buscador=async(req,res)=>{
-  barra=document.getElementById("ctn-bars-search");
-  cover=document.getElementById("cover-ctn-search");
-  input=document.getElementById("doctor")
-  box_search=document.getElementById("caja-busqueda")
-
-  filtro=input.value.toUpperCase();
-  li=box_search.getElementByTagName("li")
-
-  //filtro de elementos
-  for(i=0;i<li.lengh;i++){
-    a=li[i].getElementByTagName("a")[0]
-    textValue=a.textContent || a.innerText;
-
-    if(textValue.toUpperCase().indexOf(filtro)>-1){
-      li[i].style.display="";
-      box_search.style.display="block";
-    }
-
-  }
-
-
-}
+export const enviar = async (req, res) => {
+  const especialidad = req.body.especialidad;
+  console.log(especialidad);
+  res.redirect(`/kapu?especialidad=${encodeURIComponent(especialidad)}`);
+};
--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { principal } from "../controllers/indexFunc.js";
+import { enviar } from "../controllers/indexFunc.js";
 
 const router = Router();
 
 router.get('/', principal);
+router.post("/",enviar)
+
 
 export default router;
