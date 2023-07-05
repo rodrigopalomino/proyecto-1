cd23cd961a4a0cfbf8a460cf6fecd8ee67507463
rodrigopalomino <117995998+rodrigopalomino@users.noreply.github.com>
2023-07-05 18:42:46 -0500

    agregando funciones

--- src/controllers/indexFunc.js
+++ src/controllers/indexFunc.js
@@ -2,13 +2,26 @@ import { pool } from "../db.js";
 
 export const principal =async (req, res) => {
   
-  const[rows_doctor]= await pool.query("select * from doctor")
+  const [rows_doctor]= await pool.query("select * from doctor")
   
+  const especialidades = [...new Set(rows_doctor.map(doctor => doctor.especialidad))];
+
+
+  const doctoresPorEspecialidad = {};
+  rows_doctor.forEach(doctor => {
+    const { especialidad, nombre } = doctor;
+    if (doctoresPorEspecialidad[especialidad]) {
+      doctoresPorEspecialidad[especialidad].push(nombre);
+    } else {
+      doctoresPorEspecialidad[especialidad] = [nombre];
+    }
+  });
+  console.log("doctores : ",doctoresPorEspecialidad)
   const fechaActual=new Date().toISOString().split('T')[0];
 
-  const rows_doctor_id=[]
+  
 
-  res.render('index',{rows_doctor,fechaActual});
+  res.render('index',{rows_doctor,fechaActual,especialidades, doctoresPorEspecialidad});
 };
 
 export const enviar = async (req, res) => {
@@ -16,7 +29,6 @@ export const enviar = async (req, res) => {
   const doctor = req.body.doctor;
   const fecha = req.body.fecha;
 
-  // res.redirect(`kapu?especialidad=${especialidad}?doctor=${doctor}?fecha=${fecha}`)
   res.redirect(`kapu?especialidad=${especialidad}&doctor=${doctor}&fecha=${fecha}`);
 
 };
